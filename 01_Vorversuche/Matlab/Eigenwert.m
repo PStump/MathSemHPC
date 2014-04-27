14df4750055dbb8e04f487c5efd1cff98adee9d4
Pascal Stump <pascal-stump@bluewin.ch>
2014-04-27 17:34:31 +0200

    Neustart Eigenwert.m Matrix Generator erstellt, mit Test auf Summe = 1

--- 01_Vorversuche/Matlab/Eigenwert.m
+++ 01_Vorversuche/Matlab/Eigenwert.m
@@ -1,122 +1,26 @@
-%dünnbestzte matrix n x m sollte quadratisch sein f = Besetzungszahl in
-%Prozent
-mm = 6; % Matrix grösse
-m = mm * 17;  % geht bis 10^4 danach zu gross
-f = 0.05; % 0.05 sehr dünn besetzt 
-s = 1;
-ii = mm;
-a = 1; % Aufteilung Bandmatrix
-d = [1:10];
+% neuer Versuch, alles neu geschrieben
+%%%
 
+% Parameter
+n = 10; % Matrix Groesse [n*n]
+R = zeros(n); % Matrix mit Groesse [n*n]
 
-%R = sprand(m,m,f); % dünnbestezte Matrix
-R = spdiags(rand(m,2*a+1),-a:a,m,m);
-RR = R;
-RRR = zeros(m);
+% Matrix Generator (Pseudo Google Matrix; Spalten Summe = 1; Bandmatrix)
+  zufall = unifrnd(0,1); % ergibt Zufaellige float Zahl [0;1]
+  R(:,1) = [zufall; 1-zufall; zeros(n-2,1)]; % 1. Spalte befuellen (Zufallszahlen Summe=1, mit nullen auffuellen)
 
-% In Matrizen aufteilen
-tic;
-for i=1 : mm : m
-    eval(sprintf('P%d = R(i:ii, i:ii)',s));
-    s = s + 1;
-    ii = ii + mm;
-end
+  parfor spalte = 2:n-1 % Spalten fuellen von 2.er bis n-1.er
+    zufall = unifrnd(0,1); % ergibt Zufaellige float Zahl [0;1]
+	  zufall1 = unifrnd(0,1-zufall);
+	  R(:,spalte) = [zeros(spalte-2,1); zufall; zufall1; 1-zufall-zufall1; zeros(n-(spalte-2)-3,1)];
+  end
 
-% Eigenwerte berechnen
-for i=1 : 1 : m/mm
-    eval(sprintf('P%d = eigs(P%d)',i,i));
-end
-
-% eigenwerte in quadratuische Matrizen erstellen und diagonalisieren
-for i = 1 : 1 : m/mm
-    eval(sprintf('P%d = diag(P%d)',i,i));
-end
-
-% In ursprüngliche Matrix wieder einsetzen
-s = 1;
-ii = mm;
-
-for i = 1 : mm : m
-    eval(sprintf('RR(i:ii, i:ii) = P%d',s));
-    s = s + 1;
-    ii = ii + mm;
-end
-ZeitFuerAufteilung = toc;
-
-
-    % Beispiel Eigenwerte ausrechnen und Restliche matrix mit Nullen aufgefüllt
-    s = 1;
-    ii = mm;
-    for i = 1 : mm : m
-        eval(sprintf('RRR(i:ii, i:ii) = P%d',s));
-        s = s + 1;
-        ii = ii + mm;
-    end
-
-        % Beispiel mit Zusätzlichen Matrizen nach (m/2) unten und (m/2) rechts verschoben
-        RRRR = RR;
-        s = 1;
-        ii = 1.5*mm;
-        for i=4 : mm : m-10
-            eval(sprintf('PP%d = R(i:ii, i:ii)',s));
-            s = s + 1;
-            ii = ii + mm;
-        end
-
-        % Eigenwerte berechnen
-        for i=1 : 1 : (m/mm)-2
-            eval(sprintf('PP%d = eigs(PP%d)',i,i)); 
-        end
-
-        % eigenwerte in quadratuische Matrizen erstellen und diagonalisieren
-        for i = 1 : 1 : (m/mm)-2
-            eval(sprintf('PP%d = diag(PP%d)',i,i));
-        end
-         % In ursprungliche Matrix einsetzen und Eigenwerte zusammenzählen
-        s = 1;
-        ii = 1.5*mm;
-        for i = 4 : mm : m-10
-            eval(sprintf('RRRR(i:ii, i:ii) =  PP%d',s));
-            s = s + 1;
-            ii = ii + mm;
-        end
-
-
-tic;
-OriginalMatrix = eigs(R); % gibt die 6 Grössten Eigenvektoren raus --> Konvergieren nicht immer
-ZeitOriginaleMatrix = toc
-
-tic;
-ErsteBearbeitung = eigs(RR);
-ZeitBearbeiteteMatrix = toc
-
-tic;
-BearbeitungMitNullen = eigs(RRR);
-ZeitBearbeiteteMatrixMitNullen = toc
-
-tic;
-DoppelteBearbeitung = eigs(RRRR);
-ZeitBearbeiteteMatrixMitZweiMalMatrizen = toc
-
-ZeitBearbeitung =  ZeitBearbeiteteMatrix
-
-
-spy(R)
-figure
-spy(RR)
-figure
-spy(RRR)
-figure
-spy(RRRR)
-
-OriginalMatrix 
-ErsteBearbeitung 
-DifferenzZuOriginal = OriginalMatrix - ErsteBearbeitung
-BearbeitungMitNullen
-DifferenzZuOriginal = OriginalMatrix - BearbeitungMitNullen
-DoppelteBearbeitung
-DifferenzZuOriginal = OriginalMatrix - DoppelteBearbeitung
-
-%whos % zeigt grösse der Matrix an
-
-%clearvars;
+  zufall = unifrnd(0,1);
+  R(:,n) = [zeros(n-2,1); zufall; 1-zufall];
+	
+	% Test: Summe aller Spallten sollte Null sein
+  %parfor spalte = 1:n
+	%	if(sum(R(:,spalte)) > 1)
+	%	  sprintf('Error Spalte: %i Summe = %f', spalte, sum(R(:,spalte)))
+	%  end
+	%end
